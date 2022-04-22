e15aeb4e4c20e92cece9f2204524a9e1af12cb43
AlphaKeks <github@schnose.eu>
2022-04-22 13:47:11 +0200

    added /map

---
+++ commands/map.js
@@ -0,0 +1,134 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const globalFunctions = require("../globalFunctions");
+const { icon } = require("../config.json");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("map")
+		.setDescription("Get detailed information on a map.")
+		.setDefaultPermission(true)
+		.addStringOption((o) => o.setName("map").setDescription("Enter a global map.").setRequired(true)),
+
+	async execute(interaction) {
+		let reply = "";
+		let map = interaction.options.getString("map");
+		await interaction.deferReply();
+
+		async function answer(input) {
+			await interaction.editReply(input);
+		}
+
+		const globalMaps = await globalFunctions.getMapsAPI();
+		const mapsMap = new Map();
+
+		for (let i = 0; i < globalMaps.length; i++) {
+			if (globalMaps[i].name.includes(map)) {
+				map = globalMaps[i].name;
+				mapsMap.set(globalMaps[i].name, globalMaps[i].id);
+				break;
+			}
+		}
+		if (!mapsMap.get(map)) return answer({ content: "Please enter a valid map." });
+
+		let [filterSKZ, filterKZT, filterVNL] = ["❌", "❌", "❌"];
+
+		let [mapFilter, kzgoData] = await Promise.all([
+			globalFunctions.checkFilters(mapsMap.get(map), 0),
+			globalFunctions.kzgoMaps(),
+		]);
+
+		if ([mapFilter, kzgoData].includes("bad")) return answer({ content: "API Error. Please try again later." });
+
+		let SKZ, KZT, VNL;
+
+		mapFilter.forEach((i) => {
+			if (i.mode_id === 200) KZT = true;
+			else if (i.mode_id === 201) SKZ = true;
+			else if (i.mode_id === 202) VNL = true;
+		});
+
+		if (SKZ) filterSKZ = "✅";
+		if (KZT) filterKZT = "✅";
+		if (VNL) filterVNL = "✅";
+
+		for (let i = 0; i < kzgoData.length; i++) {
+			if (kzgoData[i].name == map) {
+				kzgoData = kzgoData[i];
+				break;
+			}
+		}
+
+		const createdAt = Date.parse(kzgoData.date);
+		const apiTier = kzgoData.tier;
+		let displayTier;
+		switch (apiTier) {
+			case 1:
+				displayTier = "Very Easy";
+				break;
+
+			case 2:
+				displayTier = "Easy";
+				break;
+
+			case 3:
+				displayTier = "Medium";
+				break;
+
+			case 4:
+				displayTier = "Hard";
+				break;
+
+			case 5:
+				displayTier = "Very Hard";
+				break;
+
+			case 6:
+				displayTier = "Extreme";
+				break;
+
+			case 7:
+				displayTier = "Death";
+				break;
+		}
+
+		let mappers = [];
+		for (let i = 0; i < kzgoData.mapperNames.length; i++) {
+			mappers.push(`[${kzgoData.mapperNames[i]}](https://steamcommunity.com/profiles/${kzgoData.mapperIds[i]})`);
+		}
+
+		const embed = new MessageEmbed()
+			.setColor("#7480c2")
+			.setTitle(map)
+			.setURL(`https://kzgo.eu/maps/${map}`)
+			.setThumbnail(`https://raw.githubusercontent.com/KZGlobalTeam/map-images/master/images/${map}.jpg`)
+			.setDescription(
+				`> API Tier: ${apiTier} (${displayTier})\n> Mapper(s): ${mappers.join(", ")}\n> Bonuses: ${
+					kzgoData.bonuses
+				}\n> Globalled: <t:${createdAt / 1000}:d>\n\nFilters:`
+			)
+			.addFields(
+				{
+					name: "SimpleKZ",
+					value: filterSKZ,
+					inline: true,
+				},
+				{
+					name: "KZTimer",
+					value: filterKZT,
+					inline: true,
+				},
+				{
+					name: "Vanilla",
+					value: filterVNL,
+					inline: true,
+				}
+			)
+			.setFooter({
+				text: `(͡ ͡° ͜ つ ͡͡°)7 | workshopID: ${kzgoData.workshopId} | schnose.eu/church`,
+				iconURL: icon,
+			});
+
+		return answer({ embeds: [embed] });
+	},
+};
--- globalFunctions.js
+++ globalFunctions.js
@@ -21,11 +21,299 @@ const globalFunctions = {
 		return cmdFiles;
 	},
 
+	// Convert UNIX timestamp into a nicer time format
+	convertmin: function (num) {
+		let millies = Math.floor(((num % 1) * 1000) / 1);
+		num = Math.floor(num / 1);
+		if (num == 0) return "None";
+		let hours = Math.floor(num / 60 / 60);
+		let minutes = Math.floor(num / 60) - hours * 60;
+		let seconds = num % 60;
+
+		if (hours != 0) {
+			return (
+				hours.toString().padStart(2, "0") +
+				":" +
+				minutes.toString().padStart(2, "0") +
+				":" +
+				seconds.toString().padStart(2, "0") +
+				"." +
+				millies.toString().padStart(3, "0")
+			);
+		} else {
+			return (
+				minutes.toString().padStart(2, "0") +
+				":" +
+				seconds.toString().padStart(2, "0") +
+				"." +
+				millies.toString().padStart(3, "0")
+			);
+		}
+	},
+
+	// 1000000 -> 1,000,000
+	numberWithCommas: function (x) {
+		return x.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	},
+
+	// Convert Discord @mention into user's ID
+	getIDFromMention: function (mention) {
+		if (!mention) return;
+
+		if (mention.startsWith("<@") && mention.endsWith(">")) {
+			mention = mention.slice(2, -1);
+
+			if (mention.startsWith("!")) {
+				mention = mention.slice(1);
+			}
+
+			return mention;
+		}
+	},
+
 	// Default error message
 	errMsg: function () {
 		console.log(
 			`Command: ${__filename}\nServer: ${interaction.guild.name} | ${interaction.guild.id}\nUser: ${interaction.user.tag} | ${interaction.user.id}\nChannel: ${interaction.channel.name} | ${interaction.channel.id}`
 		);
+	},
+
+	// Get API entries for all global maps
+	getMapsAPI: async function () {
+		let data;
+		await axios
+			.get("https://kztimerglobal.com/api/v2.0/maps?&is_validated=true&limit=9999")
+			.then((response) => {
+				data = response.data;
+			})
+			.catch((e) => {
+				data = "bad";
+				console.error(e);
+			});
+		return data;
+	},
+
+	// Get global mapcycle as an array
+	getMapcycle: async function () {
+		let h;
+		await axios
+			.get(`https://kzmaps.tangoworldwide.net/mapcycles/gokz.txt`)
+			.then(function (response) {
+				const maps = response.data;
+				let mapList = [];
+				mapList = maps.split("\r\n");
+				h = mapList;
+			})
+			.catch(function (e) {
+				h = "bad";
+				console.error(e);
+			});
+		return h;
+	},
+
+	// Check global filters for a map
+	checkFilters: async function (mapID, course) {
+		let h;
+		await axios
+			.get(`https://kztimerglobal.com/api/v2.0/record_filters?tickrates=128&limit=9999`, {
+				params: {
+					has_teleports: false,
+					map_ids: mapID,
+					stages: course,
+				},
+			})
+			.then((response) => {
+				h = response.data;
+			})
+			.catch((e) => {
+				h = "bad";
+				console.error(e);
+			});
+		return h;
+	},
+
+	// Get data of all maps on kzgo.eu
+	kzgoMaps: async function () {
+		let h;
+		await axios
+			.get("https://kzgo.eu/api/maps/")
+			.then((response) => {
+				h = response.data;
+			})
+			.catch((e) => {
+				console.log(e);
+				h = "bad";
+			});
+		return h;
+	},
+
+	// Get all maps that are doable in a certain mode
+	getDoableMaps: async function (runtype, mode) {
+		switch (mode) {
+			case "kz_timer":
+				mode = 200;
+				break;
+
+			case "kz_simple":
+				mode = 201;
+				break;
+
+			case "kz_vanilla":
+				mode = 202;
+				break;
+
+			default:
+				return;
+		}
+
+		let h = [];
+		await axios
+			.get("https://kztimerglobal.com/api/v2.0/record_filters?stages=0&tickrates=128&limit=9999", {
+				params: {
+					mode_ids: mode,
+					has_teleports: runtype,
+				},
+			})
+			.then((response) => {
+				response.data.forEach((i) => {
+					h.push(i.map_id);
+				});
+			})
+			.catch((e) => {
+				h = "bad";
+				console.error(e);
+			});
+		return h;
+	},
+
+	// Get a user's PB on a map
+	getDataPB: async function (steamid, runtype, mode, map, stage) {
+		let h = 0;
+		await axios
+			.get(`https://kztimerglobal.com/api/v2.0/records/top?`, {
+				params: {
+					steam_id: steamid,
+					has_teleports: runtype,
+					modes_list: mode,
+					map_name: map,
+					stage: stage,
+				},
+			})
+			.then((response) => {
+				let data = response.data[0];
+				if (data) {
+					h = data;
+				} else {
+					h = { time: 0 };
+				}
+			})
+			.catch((e) => {
+				h = "bad";
+				console.error(e);
+			});
+
+		return h;
+	},
+
+	// Get the WR on a map
+	getDataWR: async function (runtype, mode, map, stage) {
+		let h;
+		await axios
+			.get(`https://kztimerglobal.com/api/v2.0/records/top?`, {
+				params: {
+					has_teleports: runtype,
+					modes_list: mode,
+					map_name: map,
+					stage: stage,
+					limit: 1,
+				},
+			})
+			.then((response) => {
+				let data = response.data;
+				if (!response.data[0]) {
+					h = {
+						time: 0,
+					};
+				} else h = data[0];
+			})
+			.catch(function (e) {
+				h = "bad";
+				console.error(e);
+			});
+		return h;
+	},
+
+	// Get a user's most recent PB
+	getDataRS: async function (steamid, runtype, mode) {
+		let h;
+		await axios
+			.get(`https://kztimerglobal.com/api/v2.0/records/top/?`, {
+				params: {
+					has_teleports: runtype,
+					modes_list: mode,
+					steam_id: steamid,
+					tickrate: 128,
+					limit: 9999,
+					stage: 0,
+				},
+			})
+			.then((response) => {
+				let data = response.data;
+				if (!response.data[0]) {
+					h = {
+						created_on: 0,
+					};
+				} else h = data;
+			})
+			.catch((e) => {
+				h = "bad";
+				console.error(e);
+			});
+		return h;
+	},
+
+	// Takes in a time and returns the #place of that time
+	getPlace: async function (time) {
+		await axios
+			.get(`https://kztimerglobal.com/api/v2.0/records/place/${time.id}`)
+			.then((response) => {
+				data = response.data;
+				if (data) {
+					place = data;
+				} else {
+					//place = null;
+				}
+			})
+			.catch((err) => {
+				place = "bad";
+			});
+		if (place && place != "bad") place = "[#" + place + "]";
+		return place;
+	},
+
+	// Get the top 10 times on a map
+	getDataMaptop: async function (runtype, mode, map, stage) {
+		let h;
+		await axios
+			.get(`https://kztimerglobal.com/api/v2.0/records/top?`, {
+				params: {
+					has_teleports: runtype,
+					modes_list: mode,
+					map_name: map,
+					stage: stage,
+					limit: 10,
+				},
+			})
+			.then((response) => {
+				let data = response.data;
+				if (!response.data[0]) {
+					h = "no data";
+				} else h = data;
+			})
+			.catch((err) => {
+				h = "bad";
+			});
+		return h;
 	},
 };
 
